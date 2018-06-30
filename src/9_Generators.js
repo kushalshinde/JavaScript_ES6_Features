ea05003938744ef23f7712f6751242bd426e633b
Kushal Shinde <kushalshinde@gmail.com>
2018-06-30 18:38:27 -0400

    third commit

---
+++ src/9_Generators.js
@@ -0,0 +1,26 @@
+"use strict"
+
+//Generators are the functions that can be paused and then resumed
+
+function *g1(){
+    console.log('Hello');
+    yield 'Yield 1 Ran..';
+    console.log('World');
+    yield 'Yield 2 Ran..';
+
+    return 'Returned..';
+}
+
+var g = g1();
+
+//console.log(g.next().value);
+//console.log(g.next().value);
+//console.log(g.next().value);
+
+
+//---------------------------------------
+
+for(let val of g){
+    console.log(val);
+
+}
